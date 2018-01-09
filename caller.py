66f5a6ec9f49206d2cd1713f926f0a19e93ff19c
jdhurwitz <jdhurwitz@ucla.edu>
2018-01-08 19:44:01 -0800

    bot first commit

---
+++ caller.py
@@ -0,0 +1,35 @@
+from twilio.rest import TwilioRestClient
+
+#comment
+
+# Twilio phone number goes here. Grab one at https://twilio.com/try-twilio
+# and use the E.164 format, for example: "+12025551234"
+TWILIO_PHONE_NUMBER = "+18315349195"
+
+# list of one or more phone numbers to dial, in "+19732644210" format
+DIAL_NUMBERS = ""
+with open("mynum.txt", "r") as mynum:
+	DIAL_NUMBERS = str(mynum.readline())
+
+
+# URL location of TwiML instructions for how to handle the phone call
+TWIML_INSTRUCTIONS_URL = \
+  "http://static.fullstackpython.com/phone-calls-python.xml"
+
+# replace the placeholder values with your Account SID and Auth Token
+# found on the Twilio Console: https://www.twilio.com/console
+client = TwilioRestClient("AC1813d451fc7dc602de5cef4d844c3a6b", "0e07a6f842d5d5ba82f8c17cc31c4842")
+
+
+def dial_numbers(numbers_list):
+    """Dials one or more phone numbers from a Twilio phone number."""
+    for number in numbers_list:
+        print("Dialing " + number)
+        # set the method to "GET" from default POST because Amazon S3 only
+        # serves GET requests on files. Typically POST would be used for apps
+        client.calls.create(to=number, from_=TWILIO_PHONE_NUMBER,
+                            url=TWIML_INSTRUCTIONS_URL, method="GET")
+
+
+if __name__ == "__main__":
+    dial_numbers(DIAL_NUMBERS)
